5b9a72795f2188c4c14cd34f1324342ac967e2d9
Poonam-raj <p_rajput@hotmail.co.uk>
2021-06-07 15:04:22 +0100

    adding a newTrip form, with basic inputs and submit!

--- my-tinerary/src/Components/NewTrip.jsx
+++ my-tinerary/src/Components/NewTrip.jsx
@@ -1,5 +1,50 @@
+import {
+  IonInput,
+  IonContent,
+  IonItem,
+  IonItemDivider,
+  IonList,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonPage,
+  IonButton,
+} from '@ionic/react';
 import React from 'react';
 
-const NewTrip = () => <div>This is the new trips page</div>;
+const NewTrip = () => (
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Plan New Trip ⛅ </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonList>
+        <IonItemDivider>Name your trip</IonItemDivider>
+        <IonItem>
+          <IonInput type="text" placeholder="Amazing Holiday 🌞 " />
+        </IonItem>
+        <IonItemDivider>Input Start Date</IonItemDivider>
+        <IonItem>
+          <IonInput type="date" />
+        </IonItem>
+        <IonItemDivider>Input End Date</IonItemDivider>
+        <IonItem>
+          <IonInput type="date" />
+        </IonItem>
+        <IonItemDivider>Destination</IonItemDivider>
+        <IonItem>
+          <IonInput type="text" placeholder="Input Destination" />
+        </IonItem>
+        <IonItemDivider>Notes</IonItemDivider>
+        <IonItem>
+          <IonInput type="text" placeholder="Notes..." />
+        </IonItem>
+        <IonButton expand="block">Submit</IonButton>
+      </IonList>
+    </IonContent>
+  </IonPage>
+);
 
 export default NewTrip;
