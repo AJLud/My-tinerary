90704f31c362695ad859b98ff575ae6b1808582b
Poonam-raj <p_rajput@hotmail.co.uk>
2021-06-08 15:29:03 +0100

    adding test data (commented out or not in use)

--- my-tinerary/src/Components/Trips.jsx
+++ my-tinerary/src/Components/Trips.jsx
@@ -9,6 +9,29 @@ import {
 } from '@ionic/react';
 import { Link } from 'react-router-dom';
 
+// const [trips, setTrip] = useState({
+//   trips: [
+//     {
+//       trip_id: 01,
+//       trip_name: 'Big Northcoders Sesh',
+//       destination: 'Manchester',
+//       start_date: '2021-06-08T20:11:00.000Z',
+//     },
+//     {
+//       trip_id:02,
+//       trip_name: 'Middle East Mashup',
+//       destination: 'Egypt',
+//       start_date: '2021-10-11T20:11:00.000Z',
+//     },
+//     {
+//       trip_id: 03,
+//       trip_name: 'West Midlands Mix Up',
+//       destination: 'Birmingham',
+//       start_date: '2022-01-11T20:11:00.000Z',
+//     },
+//   ],
+// });
+
 const Homepage = () => (
   <>
     <IonButton>Current Trips</IonButton>
@@ -16,17 +39,23 @@ const Homepage = () => (
     <IonButton color="secondary">
       <Link to="/trips/new-trip">Start new trip!!!</Link>
     </IonButton>
+
     <IonCard>
-      <IonCardHeader>
-        <IonCardSubtitle>54 days until... </IonCardSubtitle>
-        <IonCardTitle>Spain</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        Keep close to Nature&apos;s heart... and break clear away, once in
-        awhile, and climb a mountain or spend a week in the woods. Wash your
-        spirit clean.
-      </IonCardContent>
+      <IonButton size="large">
+        <Link to="trips/:trip_id">
+          <IonCardHeader>
+            <IonCardSubtitle>54 days until... </IonCardSubtitle>
+            <IonCardTitle>Spain</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            Keep close to Nature&apos;s heart... and break clear away, once in
+            awhile, and climb a mountain or spend a week in the woods. Wash your
+            spirit clean.
+          </IonCardContent>
+        </Link>
+      </IonButton>
     </IonCard>
+
     <IonCard>
       <IonCardHeader>
         <IonCardSubtitle>194 days until... </IonCardSubtitle>
