35587466d8956e12a84188a3d8a02872fe472949
jasneastwd <jse.94@me.com>
2021-06-08 11:33:20 +0100

    adding some files to allow user verification and sign in but largely unworking currently so disabled for now

---
+++ my-tinerary/src/Components/RequireLogin.jsx
@@ -0,0 +1,10 @@
+import { useContext, React } from 'react';
+import UserContext from '../Contexts/User';
+import SignIn from './SignIn';
+
+const RequireLogin = (children) => {
+  const { user } = useContext(UserContext);
+  return user.username ? children : <SignIn />;
+};
+
+export default RequireLogin;
