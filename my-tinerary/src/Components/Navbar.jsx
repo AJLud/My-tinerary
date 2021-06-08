d6e0726bd8138d444d742c3d71a7fbe5f183887f
jasneastwd <jse.94@me.com>
2021-06-08 11:12:24 +0100

    improving navbar functionality with page navigation

--- my-tinerary/src/Components/Navbar.jsx
+++ my-tinerary/src/Components/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import { React } from 'react';
+import { Link } from 'react-router-dom';
 import { IonFab, IonFabButton, IonFabList } from '@ionic/react';
 
 const Navbar = () => (
   <IonFab vertical="bottom" horizontal="end">
     <IonFabButton>Nav</IonFabButton>
     <IonFabList side="top">
-      <IonFabButton>Profile</IonFabButton>
-      <IonFabButton>Buddies</IonFabButton>
-      <IonFabButton>Trips</IonFabButton>
+      <IonFabButton>
+        <Link to="/profile">Profile</Link>
+      </IonFabButton>
+      <IonFabButton>
+        <Link to="/buddies">Buddies</Link>
+      </IonFabButton>
+      <IonFabButton>
+        <Link to="/trips">Trips</Link>
+      </IonFabButton>
     </IonFabList>
   </IonFab>
 );
