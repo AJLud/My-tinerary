05942d364900520ecaf4d6e7c05e7c169d3a6c20
Poonam-raj <p_rajput@hotmail.co.uk>
2021-06-09 13:25:44 +0100

    finally being able to render trips from firestore onto the page

--- src/Components/Trips.jsx
+++ src/Components/Trips.jsx
@@ -1,47 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  IonCardContent,
+  IonButton,
   IonCard,
   IonCardHeader,
   IonCardTitle,
-  IonCardSubtitle,
-  IonButton,
+  IonCardContent,
 } from '@ionic/react';
 import { Link } from 'react-router-dom';
+import db from '../Config/firebase-setup';
+// import getTrips from '../api';
 
-// const [trips, setTrip] = useState({
-//   trips: [
-//     {
-//       trip_id: 01,
-//       trip_name: 'Big Northcoders Sesh',
-//       destination: 'Manchester',
-//       start_date: '2021-06-08T20:11:00.000Z',
-//     },
-//     {
-//       trip_id:02,
-//       trip_name: 'Middle East Mashup',
-//       destination: 'Egypt',
-//       start_date: '2021-10-11T20:11:00.000Z',
-//     },
-//     {
-//       trip_id: 03,
-//       trip_name: 'West Midlands Mix Up',
-//       destination: 'Birmingham',
-//       start_date: '2022-01-11T20:11:00.000Z',
-//     },
-//   ],
-// });
+const Homepage = (/* { user } */) => {
+  // const [userTrips, setUserTrips] = useState([]);
 
-const Homepage = () => (
-  <>
-    <IonButton>Current Trips</IonButton>
-    <IonButton>Previous Trips</IonButton>
-    <IonButton color="secondary">
-      <Link to="/trips/new-trip">Start new trip!!!</Link>
-    </IonButton>
+  // useEffect(() => {
+  //   setUserTrips(
+  //     (currUserTrips) => {
+  //       const newUserTrips = [...currUserTrips];
+  //       newUserTrips.trips = getTrips(user.username);
+  //       return newUserTrips;
+  //     },
+  //   );
 
-    <IonCard>
-      <IonButton size="large">
+  const [data, setData] = useState([]);
+
+  const getFirestore = () => {
+    const data1 = [];
+    db.collection('trips')
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          const dataObj = doc.data();
+          const dataId = doc.id;
+          const combined = { dataId, ...dataObj };
+          // i need this explaining lol whats going on
+          // needs adding into api.js
+          // needs adding into a different state (user specific trip in userTrips?)
+          data1.push(combined);
+        });
+        setData(data1);
+      });
+  };
+
+  useEffect(() => {
+    getFirestore();
+  }, []);
+  // }, []);
+
+  return (
+    <>
+      <IonButton>Current Trips</IonButton>
+      <IonButton>Previous Trips</IonButton>
+      <IonButton color="secondary">
+        <Link to="/trips/new-trip">Start new trip!!!</Link>
+      </IonButton>
+
+      {data.map((trip) => (
+        <IonCard key={trip.trip_name}>
+          <IonCardHeader>
+            <IonCardTitle>{trip.trip_name}</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>{trip.destination}</IonCardContent>
+        </IonCard>
+      ))}
+
+      {/* <IonCard>
+        <IonButton size="large">
         <Link to="trips/:trip_id">
           <IonCardHeader>
             <IonCardSubtitle>54 days until... </IonCardSubtitle>
@@ -53,43 +77,44 @@ const Homepage = () => (
             spirit clean.
           </IonCardContent>
         </Link>
-      </IonButton>
-    </IonCard>
+         </IonButton>
+      </IonCard>
 
-    <IonCard>
-      <IonCardHeader>
-        <IonCardSubtitle>194 days until... </IonCardSubtitle>
-        <IonCardTitle>New York</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        Keep close to Nature&apos;s heart... and break clear away, once in
-        awhile, and climb a mountain or spend a week in the woods. Wash your
-        spirit clean.
-      </IonCardContent>
-    </IonCard>
-    <IonCard>
-      <IonCardHeader>
-        <IonCardSubtitle>404 days until... </IonCardSubtitle>
-        <IonCardTitle>Ireland</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        Keep close to Nature&apos;s heart... and break clear away, once in
-        awhile, and climb a mountain or spend a week in the woods. Wash your
-        spirit clean.
-      </IonCardContent>
-    </IonCard>
-    <IonCard>
-      <IonCardHeader>
-        <IonCardSubtitle>404 days until... </IonCardSubtitle>
-        <IonCardTitle>Ireland</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        Keep close to Nature&apos;s heart... and break clear away, once in
-        awhile, and climb a mountain or spend a week in the woods. Wash your
-        spirit clean.
-      </IonCardContent>
-    </IonCard>
-  </>
-);
+      <IonCard>
+        <IonCardHeader>
+          <IonCardSubtitle>194 days until... </IonCardSubtitle>
+          <IonCardTitle>New York</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          Keep close to Nature&apos;s heart... and break clear away, once in
+          awhile, and climb a mountain or spend a week in the woods. Wash your
+          spirit clean.
+        </IonCardContent>
+      </IonCard>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardSubtitle>404 days until... </IonCardSubtitle>
+          <IonCardTitle>Ireland</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          Keep close to Nature&apos;s heart... and break clear away, once in
+          awhile, and climb a mountain or spend a week in the woods. Wash your
+          spirit clean.
+        </IonCardContent>
+      </IonCard>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardSubtitle>404 days until... </IonCardSubtitle>
+          <IonCardTitle>Ireland</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          Keep close to Nature&apos;s heart... and break clear away, once in
+          awhile, and climb a mountain or spend a week in the woods. Wash your
+          spirit clean.
+        </IonCardContent>
+      </IonCard> */}
+    </>
+  );
+};
 
 export default Homepage;
