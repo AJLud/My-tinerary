b2896d5d94c514e91cd3b05bbbb67ab06bc0e05a
jasneastwd <jse.94@me.com>
2021-06-10 11:39:28 +0100

    adding in buddies.api and updated Buddies state which in fact fetches the entire User details rather than specific Buddies, but this will be useful possibly on App.jsx once we have a login method completed, as we can then fetch their data by username and pass this down via props to each component as necessary, eg: array of buddy IDs to Buddies, name and avatar to Profile etc

---
+++ src/buddies.api.js
@@ -0,0 +1,18 @@
+import db from './Config/firebase-setup';
+
+const getBuddies = (user, setBuddies) => {
+  const buddiesArray = [];
+  db.collection('Users')
+    .where('username', '==', user.username)
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        const buddyData = doc.data();
+        buddiesArray.push(buddyData);
+        console.log(buddiesArray);
+      });
+      setBuddies(buddiesArray);
+    });
+};
+// this currently obtains the entire User object rather than Buddy specific array
+export default getBuddies;
