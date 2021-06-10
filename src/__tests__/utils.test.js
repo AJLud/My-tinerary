f279d732ce36f05d5e5417bd1be1b7671ecd9a43
Becky Newton <iambexnewton@gmail.com>
2021-06-10 12:11:08 +0100

    testing for util countdown to trip function complete

---
+++ src/__tests__/utils.test.js
@@ -0,0 +1,31 @@
+import countdown from '../utils/utils';
+
+describe('countdown', () => {
+  test('when passed a object, it will return an string', () => {
+    expect(typeof countdown({})).toBe('string');
+  });
+
+  test('when passed an empty object, it will return an empty string', () => {
+    expect(countdown({})).toBe('');
+  });
+
+  test('when passed an object containing 172,800 seconds, it will return a string of "2 Days until"', () => {
+    expect(countdown({ seconds: 172800 })).toBe('2 days until');
+  });
+
+  test('when passed an object containing 86,400 seconds, it will return a string of "1 Day until"', () => {
+    expect(countdown({ seconds: 86400 })).toBe('1 day until');
+  });
+
+  test('when passed an object containing 43,200 seconds, it will return a string of "12 hours until"', () => {
+    expect(countdown({ seconds: 43200 })).toBe('12 hours until');
+  });
+
+  test('when passed an object containing 3,600 seconds, it will return a string of "1 hour until"', () => {
+    expect(countdown({ seconds: 3600 })).toBe('1 hour until');
+  });
+
+  test('when passed an object containing 4320100 seconds, it will return a string of "50 days until"', () => {
+    expect(countdown({ seconds: 4320100 })).toBe('50 days until');
+  });
+});
