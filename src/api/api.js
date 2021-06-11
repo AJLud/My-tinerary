7605ecbffb97ecce18afec82cf48a98c757277e5
Poonam-raj <p_rajput@hotmail.co.uk>
2021-06-11 09:40:46 +0100

    remove malfunctioning getPastTripsByUser api call

--- src/api/api.js
+++ src/api/api.js
@@ -19,28 +19,4 @@ const getTripsByUser = (user, setUserTrips) => {
     });
 };
 
-// trying to make filtering function where you can pull trips that have passed curr date
-// currently returning nothing
-const getPastTripsByUser = (user, setUserTrips) => {
-  const userTripsArray = [];
-  const currDate = Math.floor(Date.now() / 1000);
-
-  db.collection('trips')
-    .where('owner', '==', user.username)
-    .where('start_date.seconds', '<=', currDate)
-    .get()
-    .then((snapshot) => {
-      snapshot.docs.forEach((doc) => {
-        const tripData = doc.data();
-        const tripId = doc.id;
-        const combinedTripIdAndData = { tripId, ...tripData };
-        userTripsArray.push(combinedTripIdAndData);
-      });
-      setUserTrips(userTripsArray);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-export { getTripsByUser, getPastTripsByUser };
+export default getTripsByUser;
