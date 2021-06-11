20b63bfc140fa786c3d868625df0cff3e211ae3a
Poonam-raj <p_rajput@hotmail.co.uk>
2021-06-11 11:26:05 +0100

    adding further tests to detemine if a trip is ongoing

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -32,31 +32,56 @@ describe('countdown', () => {
   test('when passed an object containing less than 3600 seconds, it will return a string of "Don\'t forget your toothbrush!"', () => {
     expect(countdown({ seconds: 3460 })).toBe("Don't forget your toothbrush!");
   });
-  test('when passed an object containing negative seconds, it will return an "Archived" string', () => {
-    expect(countdown({ seconds: -3460 })).toBe('Archived');
+
+  test('when passed an object containing negative seconds where the seconds number is smaller than the tripsDurationSeconds, it will return an "Ongoing - don\'t forget your postcard" string', () => {
+    expect(countdown({ seconds: -3460, tripDurationSeconds: 4000 })).toBe(
+      "Ongoing - don't forget your postcard",
+    );
+  });
+
+  test('when passed an object containing negative seconds where the seconds number is larger than the tripsDurationSeconds, it will return an "Archived" string', () => {
+    expect(countdown({ seconds: -3460, tripDurationSeconds: 2000 })).toBe(
+      'Archived',
+    );
   });
 });
 
 describe('dateDifference', () => {
   test('when passed nothing would return an object', () => {
-    expect(dateDifference()).toEqual({ seconds: 0 });
+    expect(dateDifference()).toEqual({ seconds: 0, tripDurationSeconds: 0 });
   });
 
   test('when given the current date and a trip date that is in the future (compared to current), returns an object with time difference in seconds', () => {
     const current = 1623332876;
     const futureTrip = 1624655521;
+    const endTime = 1624655521;
     const expected = futureTrip - current;
-    expect(dateDifference(current, futureTrip)).toEqual({ seconds: expected });
+    expect(dateDifference(current, futureTrip, endTime)).toEqual({
+      seconds: expected,
+      tripDurationSeconds: 0,
+    });
   });
 
   test('when given the current date and a trip date that has passed (compared to current), returns an object with time difference in seconds', () => {
     const current = 1623332876;
     const pastTrip = 1576555216;
+    const endTime = 1576555216;
     const expected = pastTrip - current;
-    expect(dateDifference(current, pastTrip)).toEqual({ seconds: expected });
+    expect(dateDifference(current, pastTrip, endTime)).toEqual({
+      seconds: expected,
+      tripDurationSeconds: 0,
+    });
+  });
+
+  test('when given the end date and start date of a trip (in seconds) it will return a object value pair with the time difference in seconds', () => {
+    const current = 1111111111;
+    const startSeconds = 1623332876;
+    const endSeconds = 1576555216;
+    const expectedOtherTimeDiff = startSeconds - current;
+    const expectedTimeDifference = endSeconds - startSeconds;
+    expect(dateDifference(current, startSeconds, endSeconds)).toEqual({
+      seconds: expectedOtherTimeDiff,
+      tripDurationSeconds: expectedTimeDifference,
+    });
   });
 });
-
-// const date = Math.floor(Date.now() / 1000);
-// console.log(typeof date);
-// gives seconds in number
