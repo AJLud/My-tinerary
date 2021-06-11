8ba2cbc8410464f01ae1e91a9a5090e1947f2299
Poonam-raj <p_rajput@hotmail.co.uk>
2021-06-11 09:43:12 +0100

    remove malfunctioning code to render archived trips on archive button click - no longer wanting this functionality, remove archive button

--- src/Components/Trips.jsx
+++ src/Components/Trips.jsx
@@ -10,51 +10,64 @@ import {
 } from '@ionic/react';
 
 import { Link, useHistory } from 'react-router-dom';
-
-import getTripsByUser from '../api';
+import getTripsByUser from '../api/api';
+import * as utils from '../utils/utils';
 
 const Homepage = ({ user }) => {
   const history = useHistory();
   const [userTrips, setUserTrips] = useState([]);
+
   useEffect(() => {
     getTripsByUser(user, setUserTrips);
   }, []);
+
   const setText = (trip) => {
     history.push(`/trips/${trip.tripId}`);
   };
+  const currDate = Math.floor(Date.now() / 1000);
+
   return (
     <>
+      {/* is this current trips button necessary now? */}
       <IonButton>Current Trips</IonButton>
-      <IonButton>Previous Trips</IonButton>
+      {/* <IonButton>Previous Trips</IonButton>  No longer displaying trips from past separately */}
       <IonButton color="secondary">
         <Link to="/trips/new-trip">Start new trip!!!</Link>
       </IonButton>
-      {userTrips.map((trip) => (
-        <IonCard
-          key={trip.trip_name}
-          color="light"
-          onClick={() => setText(trip)}
-        >
-          <IonCardHeader>
-            <IonCardTitle>{trip.trip_name}</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonCardSubtitle>
-              <h5>
-                Location:
-                {trip.destination}
-              </h5>
-            </IonCardSubtitle>
+      {userTrips.map((trip) => {
+        const tripDate = trip.start_date.seconds;
+        const timeElapsed = utils.dateDifference(currDate, tripDate);
+        console.log(trip.start_date);
+        return (
+          <IonCard
+            key={trip.trip_name}
+            color="light"
+            onClick={() => setText(trip)}
+          >
+            <IonCardHeader>
+              <IonCardSubtitle>
+                <h5>{utils.countdown(timeElapsed)}</h5>
+              </IonCardSubtitle>
+              <IonCardTitle>{trip.trip_name}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonCardSubtitle>
+                <h5>
+                  Location:
+                  {trip.destination}
+                </h5>
+              </IonCardSubtitle>
 
-            <IonCardSubtitle>
-              <h5>
-                Organised by:
-                {trip.owner}
-              </h5>
-            </IonCardSubtitle>
-          </IonCardContent>
-        </IonCard>
-      ))}
+              <IonCardSubtitle>
+                <h5>
+                  Organised by:
+                  {trip.owner}
+                </h5>
+              </IonCardSubtitle>
+            </IonCardContent>
+          </IonCard>
+        );
+      })}
     </>
   );
 };
