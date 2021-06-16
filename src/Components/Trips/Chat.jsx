cde29fafc26dc0d4b531954c3dae3cac771661b5
Poonam-raj <p_rajput@hotmail.co.uk>
2021-06-16 16:43:32 +0100

    adding chats

--- src/Components/Trips/Chat.jsx
+++ src/Components/Trips/Chat.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { IonCardSubtitle, IonContent } from '@ionic/react';
+import { useParams, useHistory } from 'react-router-dom';
+import {
+  IonCardSubtitle,
+  IonCardContent,
+  IonCard,
+  IonContent,
+  IonButton,
+} from '@ionic/react';
 import getTripById from '../../api/tripById.api';
 import BackButton from '../BackButton';
 import Loading from '../Loading';
 
-const Chat = () => {
+const Chat = ({ user }) => {
   const { tripId } = useParams();
+  const history = useHistory();
   const [currentTrip, setCurrentTrip] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
@@ -21,9 +28,44 @@ const Chat = () => {
   return (
     <IonContent>
       <BackButton />
+      <IonButton
+        onClick={() => {
+          history.push(`/trips/${tripId}`);
+        }}
+      >
+        Back To Trip
+      </IonButton>
       <IonCardSubtitle className="page-head">
         {currentTrip.trip_name}
       </IonCardSubtitle>
+      <IonCard className="other-message">
+        <IonCardSubtitle>AlexAlexAlex</IonCardSubtitle>
+        <IonCardContent>
+          <h2>I&apos;m so pumped for this break!</h2>
+        </IonCardContent>
+        <IonCardSubtitle>10 May, 11:29</IonCardSubtitle>
+      </IonCard>
+      <IonCard className="my-message">
+        <IonCardSubtitle>{user.username}</IonCardSubtitle>
+        <IonCardContent>
+          <h2>I know right, bring a brolly though ☂ </h2>
+        </IonCardContent>
+        <IonCardSubtitle>10 May, 13:45</IonCardSubtitle>
+      </IonCard>
+      <IonCard className="other-message">
+        <IonCardSubtitle>BigDave101</IonCardSubtitle>
+        <IonCardContent>
+          <h2>Rain rain go away am i right?</h2>
+        </IonCardContent>
+        <IonCardSubtitle>10 May, 11:29</IonCardSubtitle>
+      </IonCard>
+      <IonCard className="other-message">
+        <IonCardSubtitle>Poonam_1478</IonCardSubtitle>
+        <IonCardContent>
+          <h2>🤣🤣🤣 </h2>
+        </IonCardContent>
+        <IonCardSubtitle>10 May, 11:29</IonCardSubtitle>
+      </IonCard>
     </IonContent>
   );
 };
